48e8cbbbb9de35f593215d879ed4baf4e72de20c
Justin Mah <jmmxpdev@gmail.com>
2018-05-18 15:21:31 -0600

    Add documentation and comments to AdminServlet

--- src/main/java/codeu/controller/AdminServlet.java
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -6,16 +6,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/** Servlet class responsible for the admin page. */
 public class AdminServlet extends HttpServlet {
 
     private static final String ADMIN_MESSAGE = "admin_message";
 
+    /**
+     * This function fires when a user navigates to the admin page. It displays a message on the page, which differs
+     * depending on if you are logged in, and whether or not you are an admin.
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String user = (String) request.getSession().getAttribute("user");
         System.out.println("user: " + user);
 
+        /* Note: the page should always have "Welcome to the admin page!" because I later edited the index.jsp page to
+        only show the admin link if you are currently logged in as an admin. I (Justin) will refactor this code for
+        the MVP.
+        */
         if (user != null) {
             if (isAdmin(user)) {
                 request.getSession().setAttribute(ADMIN_MESSAGE, "Hello " + user + "! Welcome to the admin page!");
@@ -30,6 +39,11 @@ public class AdminServlet extends HttpServlet {
         return;
     }
 
+    /**
+     * Determines whether or not the given username is an admin.
+     * @param user representing the user currently logged into the site (null if not logged in)
+     * @return boolean
+     */
     public static boolean isAdmin(String user) {
         String[] admins = {"Cynthia", "Justin", "Sergio"};
         for (String admin : admins) {
