6021e04030c2c3d60f5950bd016e2d58d75285a8
Justin Mah <jmmxpdev@gmail.com>
2018-05-18 20:29:09 -0600

    Create Admin Page prototype tests

---
+++ src/test/java/codeu/controller/AdminServletTest.java
@@ -0,0 +1,75 @@
+package codeu.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AdminServletTest {
+
+    private AdminServlet adminServlet;
+    private HttpServletRequest mockRequest;
+    private HttpSession mockSession;
+    private HttpServletResponse mockResponse;
+    private RequestDispatcher mockRequestDispatcher;
+
+    @Before
+    public void setup() {
+        adminServlet = new AdminServlet();
+
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockSession = Mockito.mock(HttpSession.class);
+        Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+        mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/admin.jsp"))
+                .thenReturn(mockRequestDispatcher);
+
+    }
+
+    @Test
+    public void testDoGet() throws IOException, ServletException {
+        adminServlet.doGet(mockRequest, mockResponse);
+
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+    @Test
+    public void testDoGet_UserNotLoggedIn() throws IOException, ServletException {
+        Mockito.when(mockSession.getAttribute("user")).thenReturn(null);
+
+        adminServlet.doGet(mockRequest, mockResponse);
+
+        Mockito.verify(mockSession).setAttribute("admin_message", "You are not logged in!");
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+    @Test
+    public void testDoGet_UserNotAdmin() throws IOException, ServletException {
+        Mockito.when(mockSession.getAttribute("user")).thenReturn("Foobar");
+
+        adminServlet.doGet(mockRequest, mockResponse);
+
+        Mockito.verify(mockSession).setAttribute("admin_message", "You are not an admin!");
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+    @Test
+    public void testDoGet_UserIsAdmin() throws IOException, ServletException {
+        String admin = "Justin";
+        Mockito.when(mockSession.getAttribute("user")).thenReturn(admin);
+
+        adminServlet.doGet(mockRequest, mockResponse);
+
+        Mockito.verify(mockSession).setAttribute("admin_message", "Hello " + admin + "! Welcome to the admin page!");
+        Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+    }
+
+}
